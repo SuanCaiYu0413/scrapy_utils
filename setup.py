9b7f8e71fdb3ffae3e2d22854a6fecb5545d64cf
shecaoyuan <shecaoyuan@pretang.com>
2018-09-11 17:35:10 +0800

    更新setup文件

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@
 # @Author  : SuanCaiYu
 # @File    : setup
 # @Software: PyCharm
-
+from pkgutil import walk_packages
 from setuptools import setup
+
+
+def find_packages(path):
+    # This method returns packages and subpackages as well.
+    return [name for _, name, is_pkg in walk_packages([path]) if is_pkg]
+
 
 setup(name='scrapy-utils',
       version='0.0.1',
@@ -12,6 +18,6 @@ setup(name='scrapy-utils',
       author_email='suancaiyu0413@gmail.com',
       url='https://github.com/SuanCaiYu0413/scrapy_utils',
       package_dir={'': 'src'},
-      packages=['scrapy_utils'],
+      packages=list(find_packages('src')),
       license='MIT',
       keywords='scrapy-utils')
