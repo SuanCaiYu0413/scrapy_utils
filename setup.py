ebc3fc6f91c4a4a5bb8518ba7cd5f118dfbaa6fb
shecaoyuan <shecaoyuan@pretang.com>
2018-09-11 17:27:54 +0800

    创建安装脚本

---
+++ setup.py
@@ -0,0 +1,17 @@
+# @Time    : 2018/9/11 下午5:08
+# @Author  : SuanCaiYu
+# @File    : setup
+# @Software: PyCharm
+
+from setuptools import setup
+
+setup(name='scrapy-utils',
+      version='0.0.1',
+      description='a scrapy public utils project',
+      author='SuanCaiYu0413',
+      author_email='suancaiyu0413@gmail.com',
+      url='https://github.com/SuanCaiYu0413/scrapy_utils',
+      package_dir={'': 'src'},
+      packages=['scrapy_utils'],
+      license='MIT',
+      keywords='scrapy-utils')
