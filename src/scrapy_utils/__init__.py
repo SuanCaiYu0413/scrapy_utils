f8981827b9076388fa719d89f2f4ff80dc8aff8a
shecaoyuan <shecaoyuan@pretang.com>
2018-09-11 17:06:18 +0800

    添加随机UserAgent下载中间价

---
+++ src/scrapy_utils/__init__.py
@@ -0,0 +1,4 @@
+# @Time    : 2018/9/11 下午4:53
+# @Author  : SuanCaiYu
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ src/scrapy_utils/downloadermiddlewares/__init__.py
@@ -0,0 +1,4 @@
+# @Time    : 2018/9/11 下午4:54
+# @Author  : SuanCaiYu
+# @File    : __init__.py
+# @Software: PyCharm
